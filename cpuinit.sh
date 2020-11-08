546b7932ee61fbaf5f0cabd334565f58f68a8877
Griffin <gdube@g.clemson.edu>
2020-11-08 22:55:14 +0000

    adding azcopy download

--- cpuinit.sh
+++ cpuinit.sh
@@ -1,5 +1,9 @@
 #ensure PBS on path
 export PATH=$PATH:/opt/pbs/bin
+export PATH=$PATH:/tmp
+LOCAL_TAR=/tmp/azcopy.tar.gz
+wget -O ${LOCAL_TAR} https://aka.ms/downloadazcopy-v10-linux >/dev/null
+tar -xzf ${LOCAL_TAR} --strip-components 1 --wildcards **/azcopy
 
 #add mpi
 module add mpi/hpcx
