8c3c2cfdaa35b0b17aae813a77bd7e2e09537e62
Griffin Dube <46583343+gadube@users.noreply.github.com>
2020-11-10 00:02:06 -0500

    Update mpi_build_install.sh

--- mpi_build_install.sh
+++ mpi_build_install.sh
@@ -7,7 +7,7 @@ fi
 
 
 GCC_VERSION=gcc-9.2.0
-INSTALL_PREFIX=/persist
+INSTALL_PREFIX=/persist/tools
 
 # load extra libraries
 HPCX_FOLDER=hpcx-v2.7.0-gcc-MLNX_OFED_LINUX-5.1-0.6.6.0-redhat7.7-x86_64
@@ -15,8 +15,10 @@ HPCX_PATH=${INSTALL_PREFIX}/${HPCX_FOLDER}
 HCOLL_PATH=${HPCX_PATH}/hcoll
 UCX_PATH=${HPCX_PATH}/ucx
 
-OMPI_VERSION="4.0.3"
-OMPI_DOWNLOAD_URL=https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-${OMPI_VERSION}.tar.gz
+#define versions (make sure to change the URL version too)
+OMPI_VERSION="3.1.0"
+OMPI_DOWNLOAD_URL=https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-${OMPI_VERSION}.tar.gz
+INSTALL_PATH=${INSTALL_PREFIX}/openmpi-${OMPI_VERSION}
 
 # Load gcc
 export PATH=/opt/${GCC_VERSION}/bin:$PATH
@@ -24,7 +26,7 @@ export LD_LIBRARY_PATH=/opt/${GCC_VERSION}/lib64:$LD_LIBRARY_PATH
 set CC=/opt/${GCC_VERSION}/bin/gcc
 set GCC=/opt/${GCC_VERSION}/bin/gcc
 
-
+# get the source tar if it does not already exist
 if [[ ! -f openmpi-${OMPI_VERSION}.tar.gz ]]; then
   wget -O openmpi-${OMPI_VERSION}.tar.gz ${OMPI_DOWNLOAD_URL}
 fi
@@ -38,26 +40,25 @@ cd /mnt/resource
 echo "untaring..."
 tar -xf openmpi-${OMPI_VERSION}.tar.gz
 cd openmpi-${OMPI_VERSION}
-# sudo ./configure --prefix=${INSTALL_PREFIX}/openmpi-${OMPI_VERSION} --with-ucx=${UCX_PATH} --with-hcoll=${HCOLL_PATH} --enable-mpirun-prefix-by-default --with-tm=/opt/pbs --with-platform=contrib/platform/mellanox/optimized && sudo make -j$(nproc) && sudo make install
-sudo ./configure --prefix=${INSTALL_PREFIX}/openmpi-${OMPI_VERSION} --enable-mpirun-prefix-by-default --with-tm=/opt/pbs && sudo make -j$(nproc) && sudo make install
-
-echo "installed to ${INSTALL_PREFIX}/openmpi-${OMPI_VERSION}"
+sudo ./configure --prefix=${INSTALL_PATH} --with-ucx=${UCX_PATH} --with-hcoll=${HCOLL_PATH} --enable-mpirun-prefix-by-default --with-tm=/opt/pbs --with-platform=contrib/platform/mellanox/optimized && sudo make -j$(nproc) && sudo make install && echo "installed to ${INSTALL_PATH}"
+#sudo ./configure --prefix=${INSTALL_PATH} --enable-mpirun-prefix-by-default --with-tm=/opt/pbs && sudo make -j$(nproc) && sudo make install && echo "installed to ${INSTALL_PATH}"
 # create modulefile
-cat << EOF >> /usr/share/Modules/modulefiles/mpi/openmpi-${OMPI_VERSION}
+mkdir -p ${INSTALL_PREFIX}/modulefiles/mpi/
+cat << EOF >> ${INSTALL_PREFIX}/modulefiles/mpi/openmpi-${OMPI_VERSION}
 #%Module 1.0
 #
 #  OpenMPI ${OMPI_VERSION}
 #
 conflict        mpi
 module load ${GCC_VERSION}
-prepend-path    PATH            /opt/openmpi-${OMPI_VERSION}/bin
-prepend-path    LD_LIBRARY_PATH /opt/openmpi-${OMPI_VERSION}/lib
-prepend-path    MANPATH         /opt/openmpi-${OMPI_VERSION}/share/man
-setenv          MPI_BIN         /opt/openmpi-${OMPI_VERSION}/bin
-setenv          MPI_INCLUDE     /opt/openmpi-${OMPI_VERSION}/include
-setenv          MPI_LIB         /opt/openmpi-${OMPI_VERSION}/lib
-setenv          MPI_MAN         /opt/openmpi-${OMPI_VERSION}/share/man
-setenv          MPI_HOME        /opt/openmpi-${OMPI_VERSION}
+prepend-path    PATH            ${INSTALL_PATH}/bin
+prepend-path    LD_LIBRARY_PATH ${INSTALL_PATH}/lib
+prepend-path    MANPATH         ${INSTALL_PATH}/share/man
+setenv          MPI_BIN         ${INSTALL_PATH}/bin
+setenv          MPI_INCLUDE     ${INSTALL_PATH}/include
+setenv          MPI_LIB         ${INSTALL_PATH}/lib
+setenv          MPI_MAN         ${INSTALL_PATH}/share/man
+setenv          MPI_HOME        ${INSTALL_PATH}
 EOF
 
 echo "Modulefile created for mpi/openmpi-${OMPI_VERSION}"
